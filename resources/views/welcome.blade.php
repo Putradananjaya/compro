4d9e41f676746eaf33a31c7181d356e9d6be7a5e
danan jaya <wira.putra03.wp@gmail.com>
2023-06-25 11:41:11 +0800

    This is First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,345 @@
+<!DOCTYPE HTML>
+<!--
+	Dopetrope by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>A1 BALIHUB</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="{{ asset('assets/css/main.css')}}" />
+	</head>
+	<body class="homepage is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<section id="header">
+
+					<!-- Logo -->
+						<h1><a href="index.html">PT. Arta Ananta Dinala</a></h1>
+                        <!-- <h4><a href="index.html">PT. Arta Ananta Dinala</a></h4> -->
+
+					<!-- Nav -->
+						<nav id="nav">
+							<ul>
+								<li class="current"><a href="index.html">Home</a></li>
+								<li><a href="left-sidebar.html">About Us</a></li>
+								<li><a href="right-sidebar.html">Services</a></li>
+								<li><a href="no-sidebar.html">Portofolio</a></li>
+                                <li><a href="no-sidebar.html">Blog</a></li>
+                                <li>
+									<a href="#">Contact Us</a>
+									<ul>
+										<li><a href="#">Lorem ipsum dolor</a></li>
+										<li><a href="#">Magna phasellus</a></li>
+										<li><a href="#">Etiam dolore nisl</a></li>
+										<li>
+											<a href="#">Phasellus consequat</a>
+											<ul>
+												<li><a href="#">Magna phasellus</a></li>
+												<li><a href="#">Etiam dolore nisl</a></li>
+												<li><a href="#">Veroeros feugiat</a></li>
+												<li><a href="#">Nisl sed aliquam</a></li>
+												<li><a href="#">Dolore adipiscing</a></li>
+											</ul>
+										</li>
+										<li><a href="#">Veroeros feugiat</a></li>
+									</ul>
+								</li>
+							</ul>
+						</nav>
+
+					<!-- Banner -->
+						<section id="banner">
+							<header>
+								<h2>A1 BaliHub. Making challenges possible.</h2>
+								<p>The Perfect Tech Experience</p>
+							</header>
+						</section>
+
+					<!-- Intro -->
+						<section id="intro" class="container">
+							<div class="row">
+								<div class="col-4 col-12-medium">
+									<section class="first">
+										<i class="icon solid featured fa-home"></i>
+										<header>
+											<h2>A1 Bali Property</h2>
+										</header>
+										<p align= "justify">This business is in great demand because the need for housing, business space, and property prices will always increase from time to time.</p>
+									</section>
+								</div>
+								<div class="col-4 col-12-medium">
+									<section class="middle">
+										<i class="icon solid featured alt fa-desktop"></i>
+										<header>
+											<h2>A1 Bali Tech</h2>
+										</header>
+										<p align= "justify">Tech businesses provide products and services that have applications across multiple industries — but at their core, they pertain to the research, development and distribution of technology-based goods. </p>
+									</section>
+								</div>
+								<div class="col-4 col-12-medium">
+									<section class="last">
+										<i class="icon solid featured alt2 fa-handshake"></i>
+										<header>
+											<h2>A1 Bali Consultant</h2>
+										</header>
+										<p align= "justify">Business Consultant is a business that provides consulting services related to business and business, both to individuals and companies.</p>
+									</section>
+								</div>
+							</div>
+							<footer>
+								<ul class="actions">
+									<li><a href="#" class="button large">Get Started</a></li>
+									<li><a href="#" class="button alt large">Learn More</a></li>
+								</ul>
+							</footer>
+						</section>
+
+				</section>
+
+			<!-- Main -->
+				<section id="main">
+					<div class="container">
+						<div class="row">
+							<div class="col-12">
+
+								<!-- Portfolio -->
+									<section>
+										<header class="major">
+											<h2>A1 BaliHub Portfolio</h2>
+										</header>
+										<div class="row">
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+													<header>
+														<h3>Nyitdah Bamboo Villas Canggu</h3>
+													</header>
+													<p align= "justify">2-storey Bamboo structure with an area of ​​84m2 type with a floor combination of bamboo and ironwood, roof with bamboo plupuh covered with sheet asphalt.</p>
+													<footer>
+														<ul class="actions">
+															<li><a href="#" class="button alt">Find out more</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+													<header>
+														<h3>Eco Bamboo Sambangan</h3>
+													</header>
+													<p align= "justify">This inn has a unique concept because it carries the theme of bamboo and is surrounded by trees as if you are really in a forest.</p>
+													<footer>
+														<ul class="actions">
+															<li><a href="#" class="button alt">Find out more</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+													<header>
+														<h3>Sambangan Agro & Spa</h3>
+													</header>
+													<p align= "justify">Body spa is a body treatment that is done by soaking in water, which is usually filled with scented flowers and mineral content. After soaking, the body will get a massage to make it more relaxed.</p>
+													<footer>
+														<ul class="actions">
+															<li><a href="#" class="button alt">Find out more</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+													<header>
+														<h3>Umah Melah Banyuning</h3>
+													</header>
+													<p align= "justify">Housing and residential areas are a unified system consisting of coaching, housing administration, residential area management, maintenance and repair, prevention and improvement of the quality of slum housing and slum settlements, provision of land, funding and financing systems, and the role of the community.</p>
+													<footer>
+														<ul class="actions">
+															<li><a href="#" class="button alt">Find out more</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
+													<header>
+														<h3>Land Property</h3>
+													</header>
+													<p align= "justify">Infrastructure development is inseparable from a well-planned land acquisition process from the start. This is to ensure proper and fair land acquisition. Thus, the role of the Directorate General of Land Procurement and Land Development in the future is very crucial in supporting development.</p>
+													<footer>
+														<ul class="actions">
+															<li><a href="#" class="button alt">Find out more</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+											<div class="col-4 col-6-medium col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
+													<header>
+														<h3>Sistem Informasi Koperasi Side Ayu</h3>
+													</header>
+													<p align= "justify">The Digital Cooperative Application is a cloud-based online information system software dedicated to assisting in the management & administration of your cooperative. So that the cooperative becomes more professional.</p>
+													<footer>
+														<ul class="actions">
+															<li><a href="#" class="button alt">Find out more</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+										</div>
+									</section>
+
+							</div>
+							<div class="col-12">
+
+								<!-- Blog -->
+									<section>
+										<header class="major">
+											<h2>The Blog</h2>
+										</header>
+										<div class="row">
+											<div class="col-6 col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic08.jpg" alt="" /></a>
+													<header>
+														<h3>Balinese Culture and Traditions</h3>
+														<p>Posted 45 minutes ago</p>
+													</header>
+													<p align= "justify">Heading to Bali and looking for an adventurous day trip? You can’t miss a day (or two!) discovering some of Bali’s best hidden gems a little off the beaten track – waterfalls. No matter where you’re based, we’ve sourced the best waterfalls worth visiting on your next trip to the island paradise. The Balinese people follow a form of Hinduism known as Agama Hindu Dharma. Balinese culture and traditions religion impacts almost every aspect of life on the island and draws people to Bali to see and experience it.</p>
+													<footer>
+														<ul class="actions">
+															<li><a href="#" class="button icon solid fa-file-alt">Continue Reading</a></li>
+															<li><a href="#" class="button alt icon solid fa-comment">33 comments</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+											<div class="col-6 col-12-small">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic09.jpg" alt="" /></a>
+													<header>
+														<h3>The Influence Of Work Life Balance On Job Satisfaction Of Housekeeping Employee</h3>
+														<p>Posted 45 minutes ago</p>
+													</header>
+													<p align= "justify">Human resources are the main capital to achieve company’s goals in hospitality industry. One important thing that company should consider in maintaining employee performance is a job satisfaction. Employee that does not have a job satisfaction while working will gradually decrease the employee performance. Many factors influence job satisfaction one of them is work life balance. The low work life balance resulted a low job satisfaction. This research aims at investigating </p>
+													<footer>
+														<ul class="actions">
+															<li><a href="#" class="button icon solid fa-file-alt">Continue Reading</a></li>
+															<li><a href="#" class="button alt icon solid fa-comment">33 comments</a></li>
+														</ul>
+													</footer>
+												</section>
+											</div>
+										</div>
+									</section>
+
+							</div>
+						</div>
+					</div>
+				</section>
+
+			<!-- Footer -->
+				<section id="footer">
+					<div class="container">
+						<div class="row">
+							<div class="col-4 col-6-medium col-12-small">
+                            <section>
+									<header>
+										<h2>What's this all about?</h2>
+									</header>
+									<a href="#" class="image featured"><img src="images/pic10.jpg" alt="" /></a>
+									<p>
+										This is <strong>Dopetrope</strong> a free, fully responsive HTML5 site template by
+										<a href="http://twitter.com/ajlkn">AJ</a> for <a href="http://html5up.net/">HTML5 UP</a> It's released for free under
+										the <a href="http://html5up.net/license/">Creative Commons Attribution</a> license so feel free to use it for any personal or commercial project &ndash; just don't forget to credit us!
+									</p>
+									<footer>
+										<ul class="actions">
+											<li><a href="#" class="button">Find out more</a></li>
+										</ul>
+									</footer>
+								</section>
+							</div>
+							<div class="col-4 col-6-medium col-12-small">
+								<section>
+									<header>
+										<h2>Navigation</h2>
+									</header>
+									<ul class="divided">
+										<li><a href="#">Home</a></li>
+										<li><a href="#">About Us</a></li>
+										<li><a href="#">Services</a></li>
+										<li><a href="#">Portfolio</a></li>
+										<li><a href="#">Blog</a></li>
+										<li><a href="#">Contact Us</a></li>
+									</ul>
+								</section>
+							</div>
+							<div class="col-4 col-12-medium">
+								<section>
+									<header>
+										<h2>Contact</h2>
+									</header>
+									<ul class="social">
+										<li><a class="icon brands fa-facebook-f" href="#"><span class="label">Facebook</span></a></li>
+										<li><a class="icon brands fa-twitter" href="#"><span class="label">Twitter</span></a></li>
+										<li><a class="icon brands fa-instagram" href="#"><span class="label">Instagram</span></a></li>
+										<li><a class="icon brands fa-linkedin-in" href="#"><span class="label">LinkedIn</span></a></li>
+									</ul>
+									<ul class="contact">
+										<li>
+											<h3>Address</h3>
+											<p>
+                                                  
+                                                Celukbuluh, Jl. Desa Kalibukbuk-Anturan, Anturan, <br />
+                                                Kec. Buleleng, Kabupaten Buleleng,<br />
+                                                Bali 81119
+											</p>
+										</li>
+										<li>
+											<h3>Mail</h3>
+											<p><a href="#">a1balihub@gmail.com</a></p>
+										</li>
+										<li>
+											<h3>Phone</h3>
+											<p>(+62) 8677-6210-0752</p>
+										</li>
+									</ul>
+								</section>
+							</div>
+							<div class="col-12">
+
+								<!-- Copyright -->
+									<div id="copyright">
+										<ul class="links">
+											<li>&copy; PT. Arta Ananta Dinala. All rights reserved.</li><li>Design: <a href="http://html5up.net">A1 Bali Tech</a></li>
+										</ul>
+									</div>
+
+							</div>
+						</div>
+					</div>
+				</section>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+			<script src="{{ asset('assets/js/jquery.dropotron.min.js') }}"></script>
+			<script src="{{ asset('assets/js/browser.min.js') }}"></script>
+			<script src="{{ asset('assets/js/breakpoints.min.js') }}"></script>
+			<script src="{{ asset('assets/js/util.js') }}"></script>
+			<script src="{{ asset('assets/js/main.js') }}"></script>
+
+	</body>
+</html>
